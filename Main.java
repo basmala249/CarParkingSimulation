ed684091520bdb157364396f453fec0607793de8
Basmala Ahmed Ali Ibrahim <117600939+basmala249@users.noreply.github.com>
2024-11-09 01:16:07 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,4 @@
 package org.example;
-
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
@@ -11,7 +10,7 @@ public class Main {
     static public int ParkingSlots = 0;
     public static Semaphore parkingSemaphore = new Semaphore(4);  // Semaphore for parking slots
     public static HashMap<Integer, Semaphore> GateSemaphore = new HashMap<>();  // Semaphore for gates
-
+    public static Semaphore inCdeC = new Semaphore(1);
     public static void Insert() {
         Semaphore sem1 = new Semaphore(1);
         Semaphore sem2 = new Semaphore(1);
@@ -20,25 +19,29 @@ public class Main {
         GateSemaphore.put(2, sem2);
         GateSemaphore.put(3, sem3);
     }
+
+
+
     public static void inCParkingSlots() {
         ParkingSlots++;
     }
+
     public static void deCParkingSlots() {
         ParkingSlots--;
     }
+
     public static void main(String[] args) {
         Insert();
         String filePath = "C:\\Users\\Lenovo\\Downloads\\AssigmentOS2\\src\\main\\java\\org\\example\\file.txt"; // Replace with the actual file path
-        ArrayList <Car> upComingCars = new ArrayList<>();
-        upComingCars = fileRead(filePath);
+        ArrayList<Car> upComingCars = fileRead(filePath);
         for (Car car : upComingCars) {
-                MultiThreading thready = new MultiThreading(car);
-                thready.start();
+            MultiThreading thready = new MultiThreading(car);
+            thready.start();
         }
     }
+
     public static ArrayList<Car> fileRead(String filePath){
         ArrayList<Car> upComingCars = new ArrayList<Car>();
-
 
         try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
             String line;
@@ -59,10 +62,11 @@ public class Main {
                 upComingCars.add(car);
             }
         } catch (IOException e) {
-                System.out.println("An error occurred while reading the file: " + e.getMessage());
+            System.out.println("An error occurred while reading the file: " + e.getMessage());
         }
         return upComingCars;
     }
+
     public static void switchFunction(Car car, int i, int val) {
         switch (i) {
             case 0:
