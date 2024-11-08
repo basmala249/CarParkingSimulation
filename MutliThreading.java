95820d8b80fb8df05187a8474b946bf018a91f90
Basmala Ahmed Ali Ibrahim <117600939+basmala249@users.noreply.github.com>
2024-11-09 01:16:32 +0200

    Update MutliThreading.java

--- MutliThreading.java
+++ MutliThreading.java
@@ -1,5 +1,4 @@
 package org.example;
-
 import java.util.concurrent.Semaphore;
 
 public class MultiThreading extends Thread {
@@ -17,24 +16,28 @@ public class MultiThreading extends Thread {
             System.out.println("Car " + ThisCar.getCarNumber() + " from Gate " + ThisCar.getGateNumber() + " arrived at time " + ThisCar.getArrivalTime());
             Semaphore semGate = Main.GateSemaphore.get(ThisCar.getGateNumber());
             semGate.acquire();
-            System.out.println("Avalialbe : " + Main.parkingSemaphore.availablePermits());
+            boolean waited = false;
             if (Main.parkingSemaphore.availablePermits() <= 0) {
+                waited = true;
                 System.out.println("Car " + ThisCar.getCarNumber() + " from Gate " + ThisCar.getGateNumber() + " waiting for a spot.");
-                Main.parkingSemaphore.acquire();
-                semGate.release();
-                Main.inCParkingSlots();
+            }
+            Main.parkingSemaphore.acquire();
+            Main.inCdeC.acquire();
+            Main.inCParkingSlots();
+            if(waited) {
                 System.out.println("Car " + ThisCar.getCarNumber() + " from Gate " + ThisCar.getGateNumber() + " parked after waiting for " + ThisCar.getWaitingTime() +" units of time. (Parking Status: " + Main.ParkingSlots + " spots occupied)");
             }
             else {
-                Main.parkingSemaphore.acquire();
-                semGate.release();
-                Main.inCParkingSlots();
                 System.out.println("Car " + ThisCar.getCarNumber() + " from Gate " + ThisCar.getGateNumber() + " parked. (Parking Status: " + Main.ParkingSlots + " spots occupied)");
             }
+            Main.inCdeC.release();
+            semGate.release();
             Thread.sleep(ThisCar.getParkingTime() * 1000L);
-            Main.parkingSemaphore.release();
+            Main.inCdeC.acquire();
             Main.deCParkingSlots();
             System.out.println("Car " + ThisCar.getCarNumber() + " from Gate " + ThisCar.getGateNumber() + " left after " + ThisCar.getParkingTime() + " units of time. (Parking Status: " + Main.ParkingSlots + " spots occupied)");
+            Main.inCdeC.release();
+            Main.parkingSemaphore.release();
         } catch (InterruptedException e) {
             System.err.println("Car " + ThisCar.getCarNumber() + " was interrupted during its operation.");
             Thread.currentThread().interrupt();
